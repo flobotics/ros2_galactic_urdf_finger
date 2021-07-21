aae23c9a95429f8e1c46ce21c869648bff8f131c
fk <inflo@web.de>
2021-07-21 20:46:06 +0200

    added meshes

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.py')),
-        (os.path.join('share', package_name), glob('urdf/*'))
+        (os.path.join('share', package_name), glob('urdf/*')),
+        (os.path.join('share', package_name), glob('meshes/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
